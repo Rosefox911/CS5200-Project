b9e731c9871410b78dd9879d76a0c2b8d01ab230
Jorge Delgado <rosefox911@gmail.com>
2014-12-04 15:28:48 -0500

    Ading web services

---
+++ Project-CS5200/src/api/SiteWebService.java
@@ -0,0 +1,34 @@
+package api;
+
+import java.util.List;
+
+import javax.ws.rs.Consumes;
+import javax.ws.rs.DELETE;
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import sql.*;
+
+// api/site
+@Path("/site")
+public class SiteWebService {
+	
+	CommentDAO commentdao = CommentDAO.getInstance();
+	FollowDAO followdao = FollowDAO.getInstance();
+	LikeDAO likedao = LikeDAO.getInstance();
+	UserDAO userdao = UserDAO.getInstance();
+	
+	
+	@GET
+	@Produces("application/json")
+	@Path("/{username}")
+	public User getUserbyUsername(@PathParam("username") String username) {
+		return userdao.findUser(username);
+	}
+
+	
+
+}
